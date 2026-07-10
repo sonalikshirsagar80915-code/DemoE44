e0d08f50ce318829a6dff307387b0de551d9c2dd
Sonali kshirsagar <Sonali kshirsagar@LAPTOP-PD3IQLM3>
2026-07-10 19:37:35 +0530

    Commited

---
+++ src/test/java/Demo/DemoTest.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+      @Test(groups="smoke")
+      public void movieTest()
+      {
+    	  System.out.println("Toxic");
+      }
+}
---
+++ src/test/java/Demo/DemoTest1.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest1 
+{
+	public class DemoTest 
+	{
+	      @Test(groups="regression")
+	      public void movieTest()
+	      {
+	    	  System.out.println("Cocktail");
+	      }
+	}
+}
