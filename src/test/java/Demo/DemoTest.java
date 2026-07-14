3245f549db4a0edd581b1b199137f7e0473b3e1f
Sonali kshirsagar <Sonali kshirsagar@LAPTOP-PD3IQLM3>
2026-07-14 20:01:51 +0530

    Commited

--- src/test/java/Demo/DemoTest.java
+++ src/test/java/Demo/DemoTest.java
@@ -2,11 +2,14 @@ package Demo;
 
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class DemoTest 
 {
       @Test(groups="smoke")
       public void movieTest()
       {
     	  System.out.println("Toxic");
+    	  System.out.println("Kgf");
       }
 }
